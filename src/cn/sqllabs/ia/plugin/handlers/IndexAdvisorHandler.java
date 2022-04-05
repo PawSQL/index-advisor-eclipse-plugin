106538332897921181d8cf5a2d7fa95ad5e5e040
outjia <kaoyanzone@hotmail.com>
2022-04-05 18:06:48 +0800

    move console binding to Activator fix bugs for PackageFragment and PackageFragmentRoot.

--- src/cn/sqllabs/ia/plugin/handlers/IndexAdvisorHandler.java
+++ src/cn/sqllabs/ia/plugin/handlers/IndexAdvisorHandler.java
@@ -39,7 +39,142 @@ public class IndexAdvisorHandler extends AbstractHandler {
 
 	Config config;
 
-	public static String getOverlayedPreferenceValue(IPreferenceStore store, IResource resource, String pageId,
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IProject project = null;
+		String resultPath = null;
+		String sourcePath = null;
+		IFile resultFile = null;
+
+		Object selectItem = null;
+		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService()
+				.getSelection();
+		if (selection instanceof IStructuredSelection) {
+			selectItem = ((IStructuredSelection) selection).getFirstElement();
+		}
+
+		String fileName = "/ia_index.sql";
+		if (selectItem instanceof IFile) {
+			IFile file = (IFile) selectItem;
+			project = file.getProject();
+			sourcePath = file.getRawLocation().toOSString();
+
+			fileName = file.getName();
+			fileName = fileName.substring(0, fileName.lastIndexOf('.'));
+			fileName = fileName.concat("_ia_index.sql");
+
+			IContainer container = file.getParent();
+			resultFile = container.getFile(new Path(fileName));
+			resultPath = resultFile.getRawLocation().toOSString();
+		} else if (selectItem instanceof IFolder) {
+			// fold
+			IFolder fold = (IFolder) selectItem;
+			project = fold.getProject();
+			sourcePath = fold.getRawLocation().toOSString();
+
+			String path = fold.getRawLocation().toOSString();
+			resultPath = path.concat(fileName);
+			// create file in eclipse ui
+			resultFile = fold.getFile(new Path(fileName));
+		} else if (Adapters.adapt(selectItem, IProject.class) != null) {
+			// project
+			project = Adapters.adapt(selectItem, IProject.class);
+			String path = project.getLocation().toOSString();
+			sourcePath = path;
+
+			resultPath = path.concat(fileName);
+			// create file in eclipse ui
+			resultFile = project.getFile(new Path(fileName));
+		} else if (selectItem instanceof IPackageFragment) {
+			IPackageFragment pf = (IPackageFragment) selectItem;
+			project = pf.getJavaProject().getProject();
+			String path = pf.getResource().getRawLocation().toOSString();
+			sourcePath = path;
+
+			resultPath = path.concat(fileName);
+//			pf.getResource().getFullPath().append(fileName).toString();
+			// create file in eclipse ui
+			resultFile = project.getParent().getFile(new Path(pf.getResource().getFullPath().append(fileName).toString()));
+		} else if (selectItem instanceof IPackageFragmentRoot) {
+			IPackageFragmentRoot pfr = (IPackageFragmentRoot) selectItem;
+			project = pfr.getJavaProject().getProject();
+			String path = pfr.getResource().getRawLocation().toOSString();
+			sourcePath = path;
+
+			resultPath = path.concat(fileName);
+			// create file in eclipse ui
+			resultFile = project.getParent().getFile(new Path(pfr.getResource().getFullPath().append(fileName).toString()));
+		}
+		if (project == null || resultFile == null)
+			return null;
+
+		try {
+			InputStream stream = new ByteArrayInputStream("".getBytes());
+			if (resultFile.exists()) {
+				resultFile.setContents(stream, true, true, null);
+			} else {
+				resultFile.create(stream, true, null);
+			}
+			stream.close();
+		} catch (IOException | CoreException e) {
+		}
+
+		Config config = getConfig(project);
+		config.setResultFolder(resultPath);
+		config.setQueryFolder(sourcePath);
+		config.setMapperFolder(sourcePath);
+		config.setStatsMode("none");
+		config.setLandFlag(false);
+
+		IndexAdvisor ia = new IndexAdvisor(config);
+		ia.loadStatsAndDDL();
+		ia.recommendIndexes();
+
+		// open recommended index file after running index advisor
+		IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+		try {
+			IDE.openEditor(workbenchWindow.getActivePage(), resultFile);
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+
+	private Config getConfig(IProject project) {
+		Config config = new Config();
+		config.setDdlMode("online");
+		config.setExecMode("workload");
+
+		String pageId = IAPropertiesConstants.PAGE_ID;
+		IPreferenceStore ps = PlatformUI.getPreferenceStore();
+		String dbType = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_TYPE);
+		String host = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_HOST);
+		String user = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_USER);
+		String pwd = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_PWD);
+		String database = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DATABASE);
+		String schemaList = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.SCHEMALIST);
+
+		String queryMode = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.QUERY_MODE);
+//		String sqlFolder = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.SQL_FOLDER);
+
+		String validate = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.VALIDATE_FLAG);
+		String dedup = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DEDUP_FLAG);
+		ServerInfo server = new ServerInfo(host, user, pwd, database, dbType);
+		config.setQueryMode(queryMode);
+//		config.setMapperFolder(sqlFolder);
+//		config.setQueryFolder(sqlFolder);
+		config.setServer(server);
+		config.setDatabaseList(Arrays.asList(schemaList.split(",")));
+		config.getServer().setSchemaList(config.getDatabaseList());
+		config.setDedupExistingFlag(dedup);
+		config.setValidate(validate);
+		return config;
+	}
+	
+
+	private static String getOverlayedPreferenceValue(IPreferenceStore store, IResource resource, String pageId,
 			String name) {
 		IProject project = resource.getProject();
 		String value = null;
@@ -62,138 +197,5 @@ public class IndexAdvisorHandler extends AbstractHandler {
 		} catch (CoreException e) {
 		}
 		return null;
-	}
-
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IProject project = null;
-		String resultPath= null;
-		String sourcePath = null;
-		IFile resultFile = null;
-		
-		Object selectItem = null;
-		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService()
-				.getSelection();
-		if (selection instanceof IStructuredSelection) {
-			selectItem = ((IStructuredSelection) selection).getFirstElement();
-		}
-
-		String fileName = "/ia_index.sql";
-		if (selectItem instanceof IFile) {
-			IFile file = (IFile)selectItem;
-			project = file.getProject();
-			sourcePath = file.getRawLocation().toOSString();
-			
-			fileName = file.getName();
-			fileName = fileName.substring(0,fileName.lastIndexOf('.'));
-			fileName = fileName.concat("_ia_index.sql");
-			
-			IContainer container = file.getParent();
-			resultFile = container.getFile(new Path(fileName));
-			resultPath = resultFile.getRawLocation().toOSString();
-		}else if(selectItem instanceof IFolder){
-			//fold
-			IFolder fold = (IFolder)selectItem;
-			project = fold.getProject();
-			sourcePath = fold.getRawLocation().toOSString();
-			
-			String path = fold.getRawLocation().toOSString();
-			resultPath = path.concat(fileName);
-			//create file in eclipse ui
-			resultFile = fold.getFile(new Path(fileName));
-		}else if(Adapters.adapt(selectItem, IProject.class)!=null){
-			//project
-			project = Adapters.adapt(selectItem, IProject.class);
-			String path = project.getLocation().toOSString();
-			sourcePath = path;
-			
-			resultPath = path.concat(fileName);
-			//create file in eclipse ui
-			resultFile = project.getFile(new Path(fileName));
-		}else if(selectItem instanceof IPackageFragment){
-			IPackageFragment pf = (IPackageFragment)selectItem;
-			project = pf.getJavaProject().getProject();
-			String path = pf.getResource().getRawLocation().toOSString();
-			sourcePath = path;
-			
-			resultPath = path.concat(fileName);
-			pf.getResource().getFullPath().append(fileName).toString();
-			//create file in eclipse ui
-			resultFile = project.getFile(new Path(pf.getResource().getFullPath().append(fileName).toString()));
-		}else if(selectItem instanceof IPackageFragmentRoot){
-			IPackageFragmentRoot pfr = (IPackageFragmentRoot)selectItem;
-			project = pfr.getJavaProject().getProject();
-			String path = pfr.getResource().getRawLocation().toOSString();
-			sourcePath = path;
-			
-			resultPath = path.concat(fileName);
-			//create file in eclipse ui
-			resultFile = project.getFile(new Path(fileName));
-		}
-		if (project == null || resultFile ==null)
-			return null;
-		
-		
-		try {
-			InputStream stream = new ByteArrayInputStream("".getBytes());
-			if (resultFile.exists()) {
-					resultFile.setContents(stream, true, true, null);
-			} else {
-				resultFile.create(stream, true, null);
-			}
-			stream.close();
-		} catch (IOException | CoreException e) {
-		}
-
-		Config config = getConfig(project);
-		config.setResultFolder(resultPath);
-		config.setQueryFolder(sourcePath);
-		config.setMapperFolder(sourcePath);
-		config.setStatsMode("none");
-		config.setLandFlag(false);
-		
-		IndexAdvisor ia = new IndexAdvisor(config);
-		ia.loadStatsAndDDL();
-		ia.recommendIndexes();
-		
-		IWorkbenchWindow workbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
-
-		try {
-			IDE.openEditor(workbenchWindow.getActivePage(), resultFile);
-		} catch (PartInitException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private Config getConfig(IProject project) {
-		Config config = new Config();
-		config.setDdlMode("online");	
-		config.setExecMode("workload");
-
-		String pageId = IAPropertiesConstants.PAGE_ID;
-		IPreferenceStore ps = PlatformUI.getPreferenceStore();
-		String dbType = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_TYPE);
-		String host = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_HOST);
-		String user = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_USER);
-		String pwd = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DB_PWD);
-		String database = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DATABASE);
-		String schemaList = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.SCHEMALIST);
-		
-		String queryMode = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.QUERY_MODE);
-//		String sqlFolder = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.SQL_FOLDER);
-		
-		String validate = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.VALIDATE_FLAG);
-		String dedup = getOverlayedPreferenceValue(ps, project, pageId, IAPropertiesConstants.DEDUP_FLAG);
-		ServerInfo server = new ServerInfo(host, user, pwd, database, dbType);
-		config.setQueryMode(queryMode);
-//		config.setMapperFolder(sqlFolder);
-//		config.setQueryFolder(sqlFolder);
-		config.setServer(server);
-		config.setDatabaseList(Arrays.asList(schemaList.split(",")));
-		config.getServer().setSchemaList(config.getDatabaseList());
-		config.setDedupExistingFlag(dedup);
-		config.setValidate(validate);
-		return config;
 	}
 }
